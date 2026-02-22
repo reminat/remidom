ea1425af73dbde6ef455b35e5d469cae4721c9c2
remi <remigrz@gmail.com>
2026-02-22 19:40:23 +0100

    split terraform

---
+++ infra/terraform/envs/prod/pve/haos/main.tf
@@ -0,0 +1,44 @@
+provider "proxmox" {
+  endpoint  = var.pm_endpoint
+  api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
+  insecure  = var.pm_tls_insecure
+  ssh {
+    agent    = true
+    username = "root"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "haos" {
+  node_name = var.pm_node
+  name      = var.haos_vm_name
+
+  clone {
+    vm_id = var.haos_template_id
+  }
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.haos_cores
+    type  = "host"
+  }
+
+  memory {
+    dedicated = var.haos_memory_mb
+  }
+
+  disk {
+    datastore_id = var.vm_storage
+    interface    = "scsi0"
+    size         = var.haos_disk_gb
+  }
+
+  network_device {
+    bridge = var.vm_bridge
+  }
+
+  started = true
+  on_boot = true
+}
--- infra/terraform/envs/prod/pve/haos/variables.tf
+++ infra/terraform/envs/prod/pve/haos/variables.tf
@@ -20,31 +20,6 @@ variable "pm_tls_insecure" {
 
 variable "pm_node" { type = string }
 
-variable "vm_name" {
-  type    = string
-  default = "docker-prod-01"
-}
-
-variable "vm_template_id" {
-  type    = number
-  default = 9000
-}
-
-variable "vm_cores" {
-  type    = number
-  default = 4
-}
-
-variable "vm_memory_mb" {
-  type    = number
-  default = 8192
-}
-
-variable "vm_disk_gb" {
-  type    = number
-  default = 80
-}
-
 variable "vm_storage" {
   type    = string
   default = "local-lvm"
@@ -53,4 +28,29 @@ variable "vm_storage" {
 variable "vm_bridge" {
   type    = string
   default = "vmbr0"
+}
+
+variable "haos_vm_name" {
+  type    = string
+  default = "haos-prod-01"
+}
+
+variable "haos_template_id" {
+  type    = number
+  default = 9100
+}
+
+variable "haos_cores" {
+  type    = number
+  default = 2
+}
+
+variable "haos_memory_mb" {
+  type    = number
+  default = 4096
+}
+
+variable "haos_disk_gb" {
+  type    = number
+  default = 64
 }
